b5978a2dd4c61368a3e937b0b50219ca6878922a
Maria Jose Paz Garcia <majopazgarcia25@gmail.com>
2023-03-09 16:18:01 -0500

    first part of the input organisation, correcting errors

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './ContactUs.css';
 import emailjs from '@emailjs/browser';
 
@@ -9,15 +9,47 @@ export const ContactUs = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         //console.log(refForm.current);
-        const serviceId = "service_bjegmak"
+        const serviceId = "service_bjegmak";
         const templateId = "template_3e4qhl9";
-        const apikey =  "xw-hXJ5kXw_eJ_sHI"
-        const regex = new RegExp()
+        const apikey =  "xw-hXJ5kXw_eJ_sHI";
+        const regex = new RegExp();
+
         emailjs.sendForm(serviceId, templateId, refForm.current, apikey)
         .then( result => console.log(result.text))
         .catch (error => console.log(error))
     }
+    
+    const regexUsername = /^[a-zA-Z ]{3,50}$/;
+    const regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [userValidate, setUserValidate] = useState('');
+    const [emailValidate, setEmailValidate] = useState('');
+
+    const handleusername = (event) => {   
+        setUsername(event.target.value);
+      }
+  
+    const handleEmail = (event) => {   
+        setEmail(event.target.value);
+      }
+
+    useEffect(() => {
+    // validar username
+    if(regexUsername.test(username)){
+        setUserValidate('');
+    }else{
+        setUserValidate('Minimum 3 characters and no numbers');
+    }
+    // validar email
+    if(regexEmail.test(email)){
+        setEmailValidate('');
+    } else {
+      setEmailValidate('invalid email');
+    }
+    }, [username,email]);
+    
   return (
     <form ref={refForm} action="" onSubmit={handleSubmit}>
         <div className="header-contact">
@@ -26,11 +58,13 @@ export const ContactUs = () => {
         </div>
         <fieldset className='field-name'>
             <label className='symbol-required name'htmlFor="">Name</label>
-            <input name='username' type="text" placeholder='Ej: Maria Jose Paz' required/>
+            <input onChange={handleusername} name='username' type="text" placeholder='Ej: Maria Jose Paz' required/>
+            <p className='p-Validate'>{userValidate}</p>
         </fieldset>
         <fieldset className='field-email'>
             <label className='symbol-required' name='email'>Email</label>
-            <input type="email" placeholder='Ej: majopazgarcia25@gmail.com' name='email' id='email' required />
+            <input onChange={handleEmail} type="email" placeholder='Ej: majopazgarcia25@gmail.com' name='email' id='email' required />
+            <p className='p-Validate'>{emailValidate}</p>
         </fieldset>
         <fieldset className='field-message'>
             <label className='symbol-required'>Mensage</label>
